b5c448443a43253888e9e8888f2b7e6409841938
Xavi Duran <xavier.duran@pm.me>
2021-11-23 11:03:52 +0100

    Added stations (#16)

---
+++ beemeteo/stations/__main__.py
@@ -0,0 +1,37 @@
+import logging
+
+import click
+
+from beemeteo.stations.postal_code import PostalCode
+from beemeteo.stations.stations import Stations
+
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+@click.command()
+@click.option("--stations", type=click.File("rb"))
+@click.option("--postal-code", type=str, help="Postal code")
+@click.option("--country", type=str, default="ES", help="Country")
+def main(stations, postal_code, country):
+    """
+    Gets the closest station for a given postal code.
+    """
+
+    (latitude, longitude, distance) = PostalCode(
+        country, postal_code
+    ).find_closest(Stations.load(stations))
+    logger.info(
+        "Closest station ({latitude}, {longitude}) is {distance} "
+        "kilometers from postal code {postal_code})".format(
+            latitude=latitude,
+            longitude=longitude,
+            distance=distance,
+            postal_code=postal_code,
+        )
+    )
+
+
+if __name__ == "__main__":
+    main()
---
+++ beemeteo/stations/postal_code.py
@@ -0,0 +1,42 @@
+import pandas as pd
+
+from beemeteo.stations.coordinates import Coordinates
+
+
+class PostalCode:
+    def __init__(self, country, postal_code):
+        self.country = country
+        self.postal_code = postal_code
+        self.data = self._get()
+        self.latitude = self.data["latitude"].values[0]
+        self.longitude = self.data["longitude"].values[0]
+
+    def __str__(self):
+        return f"{self.country} {self.postal_code}"
+
+    def _get(self):
+        data = pd.read_table(
+            "beemeteo/stations/postalCode_geoLoc",
+            sep="\t",
+            names=[
+                "postalCode",
+                "latitude",
+                "longitude",
+                "altitude",
+                "country",
+                "UNK1",
+                "UNK2",
+                "UNK3",
+                "UNK4",
+                "UNK5",
+                "UNK6",
+            ],
+            dtype={"postalCode": str, "country": str},
+        )
+        return data.query(
+            "country == @self.country & postalCode == @self.postal_code"
+        )
+
+    def find_closest(self, stations):
+        building = Coordinates(self.latitude, self.longitude)
+        return building.find_closest(stations)[0]
